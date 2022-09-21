bb7c5c6e45b42172d980164ab6f32dcd41fcabfc
Venkat Appalabattula <51041363+venkatappalabattula@users.noreply.github.com>
2022-09-21 14:42:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//jQuery Smooth Scrll
+//https://www.youtube.com/watch?v=y9nlfqT4s9s&ab_channel=TraversyMedia//
+//watch this video again whenever you set the links//
+var scroll = new SmoothScroll('.popout a[href*="#"]', {
+    speed: 800
+})
